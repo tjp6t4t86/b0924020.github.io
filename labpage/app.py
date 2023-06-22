2d37addaeb41060ea9398a9fa8c192aa972976fa
tjp6t4t86 <127009626+tjp6t4t86@users.noreply.github.com>
2023-06-22 21:22:57 +0800

    VERSION_0622_2

--- labpage/app.py
+++ labpage/app.py
@@ -15,3 +15,35 @@ def store_photo():
 
 if __name__ == '__main__':
     app.run()
+
+
+
+
+
+from flask import Flask, request
+import json
+
+app = Flask(__name__)
+
+@app.route('/action_page.php', methods=['POST'])
+def process_form():
+    form_data = {
+        'fname': request.form.get('fname'),
+        'lname': request.form.get('lname'),
+        'comment': request.form.get('comment'),
+        'method': request.form.get('method'),
+    }
+    
+    # Save form data as JSON
+    with open('form_data.json', 'w') as file:
+        json.dump(form_data, file)
+    
+    # Save cropped image
+    if 'croppedImage' in request.files:
+        cropped_image = request.files['croppedImage']
+        cropped_image.save('cropped_image.jpg')
+    
+    return json.dumps({'status': 'success'})
+
+if __name__ == '__main__':
+    app.run()
